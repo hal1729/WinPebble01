da0be20c96bdafd183107450f35a50a07b73a9c8
hal1729 <hal1729@specifics.net>
2013-10-01 11:48:08 +0800

    Changes

--- src/WinPebble01.c
+++ src/WinPebble01.c
@@ -3,26 +3,107 @@
 #include "pebble_fonts.h"
 
 
-#define MY_UUID { 0xA4, 0xB9, 0xEE, 0xFF, 0x56, 0xE6, 0x4D, 0x7A, 0xB6, 0xA9, 0x43, 0x3F, 0x64, 0xAB, 0x9D, 0x31 }
+#define MY_UUID { 0xB2, 0x72, 0xEF, 0x5F, 0x8D, 0xA2, 0x49, 0xF4, 0xB0, 0x28, 0x2A, 0xAA, 0xAC, 0x7D, 0x8F, 0xB6 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "WinPebble01", "IterationOne",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+             APP_INFO_WATCH_FACE);
 
 Window window;
+
+TextLayer text_date_layer;
+TextLayer text_time_layer;
+
+Layer line_layer;
+
+
+void line_layer_update_callback(Layer *me, GContext* ctx) {
+
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+
+//  graphics_draw_line(ctx, GPoint(8, 97), GPoint(131, 97));
+ // graphics_draw_line(ctx, GPoint(8, 98), GPoint(131, 98));
+
+}
 
 
 void handle_init(AppContextRef ctx) {
 
-  window_init(&window, "Window Name");
+  window_init(&window, "Simplicity");
   window_stack_push(&window, true /* Animated */);
+  window_set_background_color(&window, GColorBlack);
+
+  resource_init_current_app(&APP_RESOURCES);
+
+
+  text_layer_init(&text_date_layer, window.layer.frame);
+  text_layer_set_text_color(&text_date_layer, GColorWhite);
+  text_layer_set_background_color(&text_date_layer, GColorClear);
+  layer_set_frame(&text_date_layer.layer, GRect(8, 28, 144-8, 40));
+  text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TEST_21)));
+  layer_add_child(&window.layer, &text_date_layer.layer);
+
+
+  text_layer_init(&text_time_layer, window.layer.frame);
+  text_layer_set_text_color(&text_time_layer, GColorWhite);
+  text_layer_set_background_color(&text_time_layer, GColorClear);
+  layer_set_frame(&text_time_layer.layer, GRect(1, 62, 144-2, 168-62));
+  text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TEST_48)));
+  layer_add_child(&window.layer, &text_time_layer.layer);
+
+
+  layer_init(&line_layer, window.layer.frame);
+  line_layer.update_proc = &line_layer_update_callback;
+  layer_add_child(&window.layer, &line_layer);
+
+
+  // TODO: Update display here to avoid blank display on launch?
+}
+
+
+void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
+
+  // Need to be static because they're used by the system later.
+  static char time_text[] = "00:00";
+  static char date_text[] = "Xxxxxxxxx 00";
+
+  char *time_format;
+
+
+  // TODO: Only update the date when it's changed.
+  string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
+  text_layer_set_text(&text_date_layer, date_text);
+
+
+  if (clock_is_24h_style()) {
+    time_format = "%R";
+  } else {
+    time_format = "%I:%M";
+  }
+
+  string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
+
+  text_layer_set_text(&text_time_layer, time_text);
+
 }
 
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
+    .init_handler = &handle_init,
+
+    .tick_info = {
+      .tick_handler = &handle_minute_tick,
+      .tick_units = MINUTE_UNIT
+    }
+
   };
   app_event_loop(params, &handlers);
 }
